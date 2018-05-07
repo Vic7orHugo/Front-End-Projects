b95b35a4330f3d10ebcf9ec6c55fa95cd3ee50b9
Vic7orHugo <Vic7orHugo@users.noreply.github.com>
2018-05-07 17:58:36 -0300

    Improved styling

--- Dota 2 Pomodoro Clock/scripts/clock_logic.js
+++ Dota 2 Pomodoro Clock/scripts/clock_logic.js
@@ -1,6 +1,5 @@
 // Template for a JavaScript (.js) file using jQuery
 
-  
 let section = {
   "br": "break",
   "sess": "session"
@@ -10,6 +9,14 @@ let state = "paused";
 let whichTimer = "session";
 let min = parseInt(document.getElementById("timer").innerHTML, 10);
 let sec = 0;
+let headStyle;
+let counterStyle;
+let count;
+
+window.onload = () => {
+  headStyle = document.createElement('STYLE');
+  document.getElementById("counter").classList.add("counter");
+}
 
 const handleClick = (event) => {
   
@@ -49,15 +56,28 @@ const handleClick = (event) => {
     case "stop-go":
       state = state === "paused" ? "counting" : "paused";
       if (state === "counting") {
+        count = parseInt(document.getElementById(whichTimer).innerHTML, 10);
+        counterColor("rgba(0, 165, 114, .8)", count*60);
+        // let count = parseInt(document.getElementById(whichTimer).innerHTML, 10);
+        
+        // if (whichTimer === "session") {
+        //   counterColor("rgba(0, 165, 114, .8)", count*60);
+        // } else {
+        //   counterColor("rgba(137, 207, 240, .8)", count*60);
+        // }
         intervalID = window.setInterval(function() {
           if (min === 0 && sec === -1 && (whichTimer === "session")) {
             whichTimer = "break";
-            min = parseInt(document.getElementById(whichTimer).innerHTML, 10);
+            count = parseInt(document.getElementById(whichTimer).innerHTML, 10);
+            counterColor("rgba(137, 207, 240, .8)", count*60);
+            min = parseInt(count, 10);
             sec = 0;
             document.getElementById("session-name").innerHTML = "Break";
           } else if (min === 0 && sec === -1 && (whichTimer === "break")) {
             whichTimer = "session";
-            min = parseInt(document.getElementById(whichTimer).innerHTML, 10);
+            count = parseInt(document.getElementById(whichTimer).innerHTML, 10);
+            counterColor("rgba(0, 165, 114, .8)", count*60);
+            min = parseInt(count, 10);
             sec = 0;
             document.getElementById("session-name").innerHTML = "Session";
           }
@@ -73,11 +93,35 @@ const handleClick = (event) => {
           }
           document.getElementById("timer").innerHTML = time;
           sec--;
-        }, 950);
+        }, 1000);
       } else if (state === "paused") {
         clearInterval(intervalID);
       }
       break;
   }
   
+};
+
+const counterColor = (color, time) => {
+  if (counterStyle) {
+    headStyle.removeChild(counterStyle);
+      console.log("fucj");
+  }
+  let counterStyleText = `
+    .counter:after {
+      content: '';
+      position: absolute;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 100%;
+      background-color: ${color};
+      animation: filler ${time}s linear;
+      z-index: -10;
+    }
+  `;
+  counterStyle = document.createTextNode(counterStyleText);
+  headStyle.appendChild(counterStyle);
+  document.head.appendChild(headStyle)
 };
